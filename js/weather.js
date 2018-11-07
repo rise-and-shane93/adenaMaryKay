08654b72fd3c742095153ddd3822c9d7b7218177
rise_and_shane93 <cdtharper@gmail.com>
2018-11-07 06:52:38 -0500

    11/7 morning commit

--- js/weather.js
+++ js/weather.js
@@ -5,6 +5,7 @@ var zipBtn = document.querySelector('.input button');
 var temp = document.createElement('li');
 var humidity = document.createElement('li');
 var pressure = document.createElement('li');
+var time = document.createElement('li');
 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=Adairsville,us&mode=json&units=imperial&APPID=c3d2c616c7f4d6abccb386c351d3a73d`).then(res => {
     return res.json();
@@ -12,10 +13,47 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=Adairsville,us&mode=jso
     temp.innerHTML = 'Temp: ' + res.main.temp + '&deg;F';
     humidity.innerHTML = 'Humidity: ' + res.main.pressure;
     pressure.innerHTML = 'Pressure: ' + res.main.humidity;
+    var hour = convertTime(res.dt);
+    time.innerHTML = 'Last updated at ' + hour;
     weatherUl.appendChild(temp);
     weatherUl.appendChild(humidity);
     weatherUl.appendChild(pressure);
+    weatherUl.appendChild(time);
 });
+
+
+
+function convertTime(timestamp) {
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    var date = new Date(timestamp*1000);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    var amPm = '';
+    
+    //sets background image based on hour
+    if (hours > 21) {
+        console.log('sleep');
+        weather.style.backgroundImage = 'url(images/weather/night.png)';
+    } else if (hours > 6 && hours < 11) {
+        console.log('morning');
+        weather.style.backgroundImage = 'url(images/weather/night.png)';
+    }
+
+    //sets the time in 12 hour format
+    if (hours > 12) {
+        // console.log('sleep');
+        amPm = ' PM';
+        hours -= 12;
+    } else {
+        amPm = ' AM';
+    }
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Will display time in 10:30:23 format
+    var formattedTime = hours + ':' + minutes.substr(-2) + amPm;
+    return formattedTime;
+};
 
 zipBtn.addEventListener('click', function() {
     var zip = zipInput.value;
