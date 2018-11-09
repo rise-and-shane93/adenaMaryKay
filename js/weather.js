d015ecbbc91c2e52b55e9d3308e4095ce903a311
rise_and_shane93 <cdtharper@gmail.com>
2018-11-09 06:37:32 -0500

    11/9/18 morning commit

--- js/weather.js
+++ js/weather.js
@@ -1,33 +1,37 @@
 var weather = document.querySelector('.weather1');
-var weatherUl = document.querySelector('.weatherUl');
+var weatherDesc = document.querySelector('.weatherDesc');
 var zipInput = document.querySelector('.input input');
 var zipBtn = document.querySelector('.input button');
-var temp = document.createElement('li');
+var temp = document.createElement('p');
 //var humidity = document.createElement('li');
 //var pressure = document.createElement('li');
-var time = document.createElement('li');
+var time = document.createElement('p');
 
+//Fetch API call that returns recent weather data for Adairsville
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=Adairsville,us&mode=json&units=imperial&APPID=c3d2c616c7f4d6abccb386c351d3a73d`).then(res => {
     return res.json();
 }).then(function(res) {
-    // var degree = 40;
-    temp.innerHTML = 'Temp: ' + res.main.temp + '&deg;F';
+    var temperature = Math.floor(res.main.temp);
+    temp.innerHTML = temperature + '&deg;F';
     //humidity.innerHTML = 'Humidity: ' + res.main.pressure;
     //pressure.innerHTML = 'Pressure: ' + res.main.humidity;
     var hour12 = convertTime12(res.dt);
     var hour24 = convertTime24(res.dt);
     console.log(hour24);
-    time.innerHTML = 'Last updated at ' + hour12;
-    weatherUl.appendChild(temp);
+    time.innerHTML = 'Last updated<br>at ' + hour12;
+    weatherDesc.appendChild(temp);
     //weatherUl.appendChild(humidity);
     //weatherUl.appendChild(pressure);
-    weatherUl.appendChild(time);
+    weatherDesc.appendChild(time);
+    temp.style.fontSize = '1.5rem';
+    temp.style.fontWeight = '900';
     
+    //Conditional-ception that sets the background image of the weather1 div based on the weather
     if (hour24 > 18 || hour24 < 6) {
         //nighttime
         if ((res.weather[0].main === 'Rain' || res.weather[0].main === 'Drizzle') && res.main.temp >= 50) {
             //you probably won't have to worry about having the temperature in the condition
-            weather.style.backgroundImage = "url('images/weather/night/rainyNight2.png')";
+            weather.style.backgroundImage = "url('images/weather/night/rainyNight.png')";
             console.log('night');
         } else if ((res.weather[0].main === 'Clear' || res.weather[0].main === 'Cloudy')&& res.main.temp >= 50) {
             weather.style.backgroundImage = "url('images/weather/night/night.png')";
@@ -61,19 +65,6 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=Adairsville,us&mode=jso
         }
     }
     weather.style.backgroundSize = 'cover';
-    /*
-    if (hour24 > 18) {
-        weather.style.backgroundImage = 'images/weather/night.png';
-        if (res.weather[0].main === 'Clear' && res.main.temp >= 50 && hour24 > 18) {
-            weather.style.backgroundImage = 'images/weather/rainy.png';
-        }
-    }
-    */
-    
-    //console.log(weatherBackgroundImage);
-    //console.log(imagePath);
-
-    //weather.style.backgroundImage = weatherBackgroundImage; 
 });
 
 function convertTime24(timestamp) {
@@ -84,10 +75,6 @@ function convertTime24(timestamp) {
     return formattedTime24;
 };
 
-function weatherBackgroundImage() {
-    console.log(weatherDesc);
-}
-
 function convertTime12(timestamp) {
     // Create a new JavaScript Date object based on the timestamp
     // multiplied by 1000 so that the argument is in milliseconds, not seconds.
@@ -96,15 +83,6 @@ function convertTime12(timestamp) {
     var hours = date.getHours();
     var amPm = '';
     
-    //sets background image based on hour
-    if (hours > 16 /*&& weatherDesc === 'Mist'*/) {
-        console.log('sleep');
-        //weather.style.backgroundImage = 'url(images/weather/cloudy.png)';
-    } else if (hours > 6 && hours < 11) {
-        console.log('morning');
-        //weather.style.backgroundImage = 'url(images/weather/night.png)';
-    }
-
     //sets the time in 12 hour format
     if (hours > 12) {
         // console.log('sleep');
