59bc27d39fba2d3f3e6fb1e0894d85fa4c6459c6
rise_and_shane93 <cdtharper@gmail.com>
2019-05-13 12:35:18 -0400

    update site

--- contact-process.php
+++ contact-process.php
@@ -12,7 +12,7 @@ if(isset($_POST['contact_form_submit'])){
     $msg = "You have received a message from: ".$contactName.", ".$contactEmail.", Message = ".$contactMessage;
 
     //the message
-    $msg = "First line of text\nSecond line of text";
+    //$msg = "First line of text\nSecond line of text";
 
     //use wordwrap() if lines are longer than 70 characters
     $msg = wordwrap($msg,70);
@@ -21,73 +21,11 @@ if(isset($_POST['contact_form_submit'])){
     mail("cdtharper@gmail.com","New Contact Form Submission",$msg);
     echo 'Email has been sent.';
     //if message successful, redirect user to thank you page
-    header('Location: thank-you.html');
+    header('Location: https://adenaharper.com/thank-you');
 
 } else {
     //kick the user from the page
     echo 'Peace out!';
-} /*
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    // access
-    $secretKey = '6LdqqYQUAAAAAOOEow81uj_bMcKprm_YBwxMshdO';
-    $captcha = $_POST['g-recaptcha-response'];
-
-    if(!$captcha){
-      echo '<p class="alert alert-warning">Please check the the captcha form.</p>';
-      exit;
-    }
-
-    # FIX: Replace this email with recipient email
-    $mail_to = "cdtharper@gmail.com";
-    
-    # Sender Data
-    //$subject = trim($_POST["subject"]);
-    $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["contact_name"])));
-    $email = filter_var(trim($_POST["contact_email"]), FILTER_SANITIZE_EMAIL);
-    //$phone = trim($_POST["phone"]);
-    $message = trim($_POST["contact_message"]);
-    
-    if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($message)) {
-        # Set a 400 (bad request) response code and exit.
-        http_response_code(400);
-        echo '<p class="alert alert-warning">Please complete the form and try again.</p>';
-        exit;
-    }
-
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
-    $responseKeys = json_decode($response,true);
-
-    if(intval($responseKeys["success"]) !== 1) {
-      echo '<p class="alert alert-warning">Please check the the captcha form.</p>';
-    } else {
-        # Mail Content
-        $content = "Name: $name\n";
-        $content .= "Email: $email\n\n";
-        //$content .= "Phone: $phone\n";
-        $content .= "Message:\n$message\n";
-
-        # email headers.
-        $headers = "From: $name <$email>";
-
-        # Send the email.
-        $success = mail($mail_to, $subject, $content, $headers);
-        if ($success) {
-            # Set a 200 (okay) response code.
-            http_response_code(200);
-            echo '<p class="alert alert-success">Thank You! Your message has been sent.</p>';
-        } else {
-            # Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo '<p class="alert alert-warning">Oops! Something went wrong, we couldnt send your message.</p>';
-        }
-    }
-
-} else {
-    # Not a POST request, set a 403 (forbidden) response code.
-    http_response_code(403);
-    echo '<p class="alert alert-warning">There was a problem with your submission, please try again.</p>';
-} */
+} 
 
 ?>
